e36ac89203c37559a8a5c7ffd628274b32d11074
Christian Talmo <talmo.christian@gmail.com>
2020-01-17 21:11:57 +0200

    feat: handle arg0 as _threshold

--- cli.js
+++ cli.js
@@ -2,6 +2,26 @@
 
 const decurse = require( './main.js' )
 
+const args = process.argv.slice( 2 )
+
+let _threshold = 0.5
+
+if ( args[ 0 ] ) {
+  const n = Number( args[ 0 ] )
+
+  if ( typeof n !== 'number' ) {
+    console.log( 'argument has to be a decimal number between 0 and 1' )
+    process.exit( 1 )
+  }
+
+  if ( Number.isNaN( n ) ) {
+    console.log( 'argument has to be a decimal number between 0 and 1' )
+    process.exit( 1 )
+  }
+
+  _threshold = n
+}
+
 let buffer = ''
 process.stdin.on( 'data', function ( chunk ) {
   buffer += chunk
